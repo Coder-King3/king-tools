a74aeb0315c2c65d717a01c1940fd7f6a3744b34
CoderKIng3 <w2196592083@gmail.com>
2023-07-27 15:44:43 +0800

    🚀feat: 新增判断数据类型函数

---
+++ src/getDataType.ts
@@ -0,0 +1,42 @@
+export function getDataType(target: any) {
+  let type = typeof target;
+  if (type === "object") {
+    return Object.prototype.toString.call(target).slice(8, -1).toLowerCase();
+  } else {
+    return type;
+  }
+}
+
+// 原始类型
+export function isString(target: any) {
+  return getDataType(target) === "string";
+}
+
+export function isNumber(target: any) {
+  return getDataType(target) === "number";
+}
+
+export function isBoolean(target: any) {
+  return getDataType(target) === "boolean";
+}
+
+export function isNull(target: any) {
+  return getDataType(target) === "null";
+}
+
+export function isUndefined(target: any) {
+  return getDataType(target) === "undefined";
+}
+
+export function isSymbol(target: any) {
+  return getDataType(target) === "symbol";
+}
+
+export function isBigInt(target: any) {
+  return getDataType(target) === "bigInt";
+}
+
+// 引用类型
+export function isObject(target: any) {
+  return getDataType(target) === "object";
+}
