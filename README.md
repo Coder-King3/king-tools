680f6d33d17d3fb07b01ad7b480165cc98d0013e
CoderKIng3 <w2196592083@gmail.com>
2023-07-28 09:52:54 +0800

    🔩config: 配置细化

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # king-tools
-king-tools库导出为CJS,ESM,UMD模块。
+
+king-tools 库导出为 ESM 模块。
 
 ## 安装
 
 使用 npm：
+
 ```shell
 $ npm i -g npm
 $ npm i king-tools
 ```
-注意：如果使用的是npm<5.0，请添加“--save”
+
+注意：如果使用的是 npm<5.0，请添加“--save”
 
 在 Node.js 中：
+
 ```js
 // Load the full build.
-var _ = require('king-tools');
+var _ = require('king-tools')
 ```
+
 ## 为什么选择 king-tools？
-Lodash让JavaScript变得更容易，因为它省去了处理数组的麻烦，数字、对象、字符串等。
+
+Lodash 让 JavaScript 变得更容易，因为它省去了处理数组的麻烦，数字、对象、字符串等。
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import resolve from "rollup-plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import resolve from 'rollup-plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
 export default {
-  input: "./main.ts", // 入口文件
+  input: './main.ts', // 入口文件
   output: [
     {
-      format: "esm", // 打包为esm格式
-      file: "dist/index.js", // 打包后的文件路径名称
-      name: "king-tools", // 打包后的默认导出文件名称
-    },
+      format: 'esm', // 打包为esm格式
+      file: 'dist/index.js', // 打包后的文件路径名称
+      name: 'king-tools' // 打包后的默认导出文件名称
+    }
   ],
-  plugins: [typescript({ tsconfig: "./tsconfig.json" }), resolve()],
-};
+  plugins: [typescript({ tsconfig: './tsconfig.json' }), resolve()]
+}
